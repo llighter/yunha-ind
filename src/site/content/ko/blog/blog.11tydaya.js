4ffdf238d1822c4587a78d314e4bb6960964dbcb
Park, Yunha <hadbsgk@gmail.com>
2019-08-01 23:22:02 +0900

    Post: set first post

---
+++ src/site/content/ko/blog/blog.11tydaya.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // All blog posts will ingerit this layout
+    // which ensures they generate the right permalinks.
+    layout: 'blog',
+    // Create a path object so things like breadcrumbs can differentiate posts
+    // coming from the blog versus other learning paths.
+    path: {
+        slug: 'blog',
+        title: 'Blog',
+        titleVariation: 'All articles',
+        description: 'My latest news, updates, and stories for developers',
+    },
+};
---
+++ src/site/content/ko/blog/my-first-post/index.md
@@ -0,0 +1,23 @@
+---
+title: "My First blog post"
+subhead: |
+  My first blog post using 11ty and Material Design Component.
+date: 2019-08-01
+authors:
+  - llighter
+hero: hero.jpeg
+alt: First Step for my website
+description: |
+  My first blog post using 11ty and Material Design Component.
+tags:
+  - post
+---
+
+## This is HEADLINE
+
+Hi my name is Park, Yunha.
+I'm the owner of this website.
+Welcome to all of you guys.
+Feel free to seeing around.
+
+Thank you.
