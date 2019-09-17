5727c686b598593aa2a89c27941c326f64e8d79d
llighter <hadbsgk@gmail.com>
2019-09-17 21:49:21 +0900

    Modify: add fit properties to hero shortcode

--- src/site/_includes/components/Hero.js
+++ src/site/_includes/components/Hero.js
@@ -18,10 +18,10 @@ const {html} = require('common-tags');
 const path = require('path');
 // const stripLanguage = require('../../_filters/strip-language');
 
-module.exports = ({page, hero, alt, heroPosition}) => {
+module.exports = ({page, hero, alt, heroPosition, heroFit = 'cover'}) => {
   return html`
     <img
-      class="w-hero ${heroPosition ? `w-hero--${heroPosition}` : ''}"
+      class="w-hero w-hero--${heroFit} ${heroPosition ? `w-hero--${heroPosition}` : ''}"
       src="${path.join(page.url, hero)}"
       alt="${alt}"
     />
