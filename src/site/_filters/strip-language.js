0f39431835c94457dc172a71ca47c9b029c7c96f
llighter <hadbsgk@gmail.com>
2019-11-13 02:13:13 +0900

    Fix: set slice index to 0 since I don't use en

--- src/site/_filters/strip-language.js
+++ src/site/_filters/strip-language.js
@@ -2,6 +2,7 @@
 // e.g. /en/foo becomes /foo.
 module.exports = (url) => {
   const urlParts = url.split('/');
-  urlParts.splice(1, 1);
+  // TODO: I don't use en or ko
+  urlParts.splice(0, 0);
   return urlParts.join('/');
 };
